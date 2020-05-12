4209bed96685d4dfa080dc2629e01291fa9b062b
Andreas <asc4asc@users.noreply.github.com>
2020-05-12 17:18:18 +0200

    Update meld.js

--- meld.js
+++ meld.js
@@ -251,3 +251,30 @@ function Double() {
       }
     }    
 }
+
+function fullscreen(){
+    elem = document.getElementsByTagName("body")[0];
+    
+    if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement){
+        if(elem.requestFullscreen){
+            elem.requestFullscreen();
+        } else if(elem.msRequestFullscreen){
+            elem.msRequestFullscreen();
+        } else if(elem.mozRequestFullScreen){
+            elem.mozRequestFullScreen();
+        } else if(elem.webkitRequestFullscreen){
+            elem.webkitRequestFullscreen(elem.ALLOW_KEYBOARD_INPUT);
+        }
+    } else {
+        if(document.exitFullscreen){
+            document.exitFullscreen();
+        } else if(document.msExitFullscreen){
+            document.msExitFullscreen();
+        } else if(document.mozCancelFullScreen){
+            document.mozCancelFullScreen();
+        } else if(document.webkitExitFullscreen){
+            document.webkitExitFullscreen();
+        }
+    }
+}
+
